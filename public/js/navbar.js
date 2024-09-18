4b3d64348b77e49539d3881902c0e8d950e9f0ca
Fescobar28 <franescobar97@hotmail.com>
2024-09-18 14:24:56 -0600

    navbar finalizado, falta dropdown

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,8 +1,13 @@
 function mostrarMenu() {
     const menu = document.getElementById("navmenu");
     const contenedor = document.getElementById("fondo-oscuro");
-    const items = document.getElementById("navitems");
-    menu.classList.toggle("visible");
-    contenedor.classList.toggle("fondo-oscuro");
-    items.classList.toggle("block");
+    menu.classList.add("visible");
+    contenedor.classList.add("fondo-oscuro");
+}
+
+function ocultarMenu(){
+    const menu = document.getElementById("navmenu");
+    const contenedor = document.getElementById("fondo-oscuro");
+    menu.classList.remove("visible");
+    contenedor.classList.remove("fondo-oscuro");
 }
